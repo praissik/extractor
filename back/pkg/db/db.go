a5ba5b1cf59e663ca91c97a90a66a5b0f259be0c
Mariusz Praiss <Mariusz.Praiss@ad.argenta.com.pl>
2021-03-25 14:35:26 +0100

    change project structure part2

---
+++ back/pkg/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+	"extractor/back/pkg/env"
+	"extractor/back/pkg/util"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+func Connect() (db *sql.DB) {
+	dbDriver := env.Get("DB_DRIVER")
+	dbServer := env.Get("DB_SERVER")
+	dbUser := env.Get("DB_USER")
+	dbPassword := env.Get("DB_PASSWORD")
+	dbName := env.Get("DB_NAME")
+
+	db, err := sql.Open(dbDriver, "server="+dbServer+";user id="+dbUser+";password="+dbPassword+";database="+dbName)
+
+	util.CheckErr(err)
+
+	return db
+}
---
+++ back/pkg/report/report.go
@@ -0,0 +1,100 @@
+package report
+
+import (
+	"database/sql"
+	"encoding/json"
+	"extractor/back/pkg/db"
+	"extractor/back/pkg/util"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/lithammer/shortuuid"
+)
+
+type Data struct {
+	IsOk       int    `json:"isOk"`
+	ReportName string `json:"reportName"`
+	FileName   string `json:"fileName"`
+}
+
+func GetReports(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println("Get names of reports")
+
+	data := util.GetReportsData()
+
+	util.SetWriter(w, 200)
+	//fmt.Println("data:", data)
+	err := json.NewEncoder(w).Encode(data)
+	util.CheckErr(err)
+}
+
+func GenerateReport(w http.ResponseWriter, r *http.Request) {
+
+	var data Data
+
+	idFV := r.FormValue("id")
+	id, _ := strconv.Atoi(idFV)
+
+	name, query := util.FetchQueryData(id)
+
+	//fmt.Println("Generate report")
+	akronim := r.FormValue("akronim")
+	kodTowaru := r.FormValue("kodTowaru")
+	symbolTowaru := r.FormValue("symbolTowaru")
+	rokOS := r.FormValue("rokOS")
+
+	db := db.Connect()
+	rows, err := db.Query(query, sql.Named("Akronim", akronim),
+		sql.Named("KodTowaru", kodTowaru),
+		sql.Named("SymbolTowaru", symbolTowaru),
+		sql.Named("RokOS", rokOS))
+	defer db.Close()
+
+	if err != nil {
+		data.IsOk = -1
+		fmt.Println(err)
+		util.CheckErr(err)
+	} else {
+
+		f := excelize.NewFile()
+		f.SetSheetName("Sheet1", "Raport")
+
+		err := util.SetCellValues(f, rows)
+
+		if err != nil {
+			data.IsOk = 0
+			fmt.Println(err)
+		} else {
+
+			uuid := shortuuid.New()
+
+			data.IsOk = 1
+			data.ReportName = name + ".xlsx"
+			data.FileName = uuid + ".xlsx"
+			url := "./front/static/" + data.FileName
+			/*
+				fmt.Println(uuid)
+				fmt.Println(data.ReportName)
+				fmt.Println(url)
+			*/
+			err := f.SaveAs(url)
+			util.CheckErr(err)
+
+			go rm(url)
+		}
+	}
+
+	util.SetWriter(w, 200)
+
+	err = json.NewEncoder(w).Encode(data)
+	util.CheckErr(err)
+}
+
+func rm(path string) {
+	time.Sleep(10000 * time.Millisecond)
+	os.Remove(path)
+}
